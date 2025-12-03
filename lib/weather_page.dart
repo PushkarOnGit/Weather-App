6602a3518e5d65f8272619479d716d98b396fd1f
PushkarOnGit <pushkarjadhav9245@gmail.com>
2025-12-03 13:19:08 +0530

    created the card and the appbar ui

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherPage extends StatefulWidget {
@@ -10,6 +12,50 @@ class WeatherPage extends StatefulWidget {
 class WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('This is the weather page')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather App'),
+        centerTitle: true,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+      ),
+      body: Column(
+        children: [
+          //CARD
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16),
+            child: SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadiusGeometry.circular(10),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text('300°F', style: TextStyle(fontSize: 32)),
+
+                          Icon(Icons.cloud, size: 64),
+                          SizedBox(height: 14),
+
+                          Text('Rain', style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+        ],
+      ),
+    );
   }
 }
