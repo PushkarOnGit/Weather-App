2a813ebee7b14040a63694a195dd4fbbda152211
PushkarOnGit <pushkarjadhav9245@gmail.com>
2025-12-03 11:54:55 +0530

    initial setup

---
+++ lib/api_key.dart
@@ -0,0 +1,4 @@
+final String cityName = 'Nagpur';
+final String apiKey = 'ea374b9bce7438cebb9a93780fbcf918';
+final apikey =
+    "http://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$apiKey";
